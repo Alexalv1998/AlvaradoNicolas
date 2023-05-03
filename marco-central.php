d4d62baa0b970ed8f222ad82680dc658e72f1971
Alexalv1998 <129921212+Alexalv1998@users.noreply.github.com>
2023-05-02 22:08:36 -0500

    Add files via upload

---
+++ marco-central.php
@@ -0,0 +1,62 @@
+<div style="float:left; width:60%; margin:60px auto; padding:1vw; border-left:1px solid gray; border-right:1px solid gray;">
+   <div  style="width: 100%;  ">     
+    <div style="width: 100%; height:100%; margin: auto ; display:inline-block;" >
+       <div style=" width:100%; height: 100%; top:0; font-size:20px; text-align:center; float:left; ">
+        	<h1>¿Quién es Nicolas A. AlvaradoMoran.?</h1>
+       		 <hr>
+             
+          <div id="posteo" style="text-align: left; font-size:0.5vw;">
+                Posteado por:  Nicolas Alvarado<br>
+                Fecha: 2023-04-30 <br>
+          </div>
+        </div>
+    </div>
+  </div>
+  <div style="width: 100%; height:100%;" >
+           <div  style="text-align:justify; font-size: 20px;">
+                   <p class="eys-parrafo">Hola, yo soy Nicolas Alvarado <b> Alvarado Moran </b>, Soy un estudiante de ingeniería en TICS, y estoy muy emocionado de estar en este campo tan apasionante y desafiante. Me encanta aprender sobre tecnología y cómo se puede utilizar para resolver problemas del mundo real. </p>
+                   <p class="eys-parrafo">Desde temprana edad me han apasionado el uso de tecnologias aprendiendo a usar dispositivos electronicos desde el hardware y software. </p>
+<p class="eys-parrafo"> El primer lenguaje con el cual empece a programar fue <b><a href="https://es.wikipedia.org/wiki/ANSI_C"> ANSI C</a></b>  el cual comence a usar cuando estudiaba en la univerdad de Guayaquil, realizando pequeños proyectos contables.</p>
+
+   <div id="blog-seccion2"  style="height: 50px;"></div>
+
+    <h2 style="border-bottom: 1px solid green;"> Mis estudios</h2> 
+    
+
+
+                   <p class="eys-parrafo">Mis estudios secundarios los realice en el colegio Fiscal Manuel Nieto Cadena anteriormente conocido Liceo Naval donde me gradue en el año 2016, de BGU (Bachillerato General Unificado), a finales de 2016 comence mis estudios universitarios en la Universidad de Guayaquil de la carrera de Ingenieria en Sistemas, por situaciones personales homologue a la univerdad tecnica Luis Vargas Torres.</p>
+                   <p class="eys-parrafo">Actualmente he adquirido conocimiento en temas de diseño grafico y artes conceptuales, mientras lo combino con mis habilidades informaticas, el cual me ha servido para trabajar de forma remota.</p>
+<p class="eys-parrafo"></p>
+   <div id="blog-seccion3"  style="height: 50px;"></div>
+
+    <h2 style="border-bottom: 1px solid green;"> Mi perfil informático</h2> 
+    
+
+
+                   <p  class="eys-parrafo">.</p> 
+                   
+   <div id="blog-seccion4"  style="height: 50px;"></div>
+
+    <h2 style="border-bottom: 1px solid green;"> Mis Hooby</h2> 
+    
+
+                   <p class="eys-parrafo">Dentro de mis hobby esta jugar videojuegos, diseñar conceptos en Adobe Illustrator y Photshop.</p>
+       
+
+                   
+
+               <hr>
+               <div style=" bottom: 0; margin-bottom: 0;   width:100%; display: inline-block;" >
+                  <div style="width: 40%;  float: left;">
+                   <a href="#">&#171;   Previo</a>
+                  </div>
+
+                  <div style="width: 40%; float: right; text-align: right;">
+                   <a href="apendiendo-cpp">Programa para aprender Phyton &#187;</a>
+                  </div>
+
+                </div>
+           </div>
+  </div> 
+
+  </div>
